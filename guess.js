c398c5b3e76fcc8ffbdc1d72bdc40c048d5b8145
Student <student@Cumberlandss-MacBook-Pro.local>
2021-10-12 10:14:23 -0500

    Created quess.js file. Also created started variables for projects.

---
+++ guess.js
@@ -0,0 +1,21 @@
+// GUESS THAT NUMBER
+// Message to be used throughout the file/project
+const enterNumText = `Please enter a number greater than zero`;
+
+// For restarting the game
+let restartGame = true;
+
+// For storing the range of the number to be guessed
+let rangeNum;
+
+// For storing the number to be guessed
+let randomNum;
+
+// For storing the number of attempts that the user has left
+let attempts;
+
+// For storing the user's guess
+let guess;
+
+// For storing user's response to play again or not play again
+let playAgain;
